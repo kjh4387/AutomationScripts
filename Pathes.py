50b0cd2e77842a1c3b832d3ead422eba08dd2f2b
kjh4387 <wkgusdl21@gmail.com>
2023-10-25 11:57:57 +0900

    departmentlist path 추가

--- Pathes.py
+++ Pathes.py
@@ -1,3 +1,6 @@
 userdata_path = './userdata.pkl'
+mailconfig_path = './mailconfig.pkl'
+
+departmentlist_path = './departmentlist.csv'
 template_path = './template.txt'
 template_variable_path = './메일문구.json'
